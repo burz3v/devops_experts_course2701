f0d0e0e454568011a6ba426dcab8191df8291854
burz3v <97345685+burz3v@users.noreply.github.com>
2023-01-27 21:39:08 +0200

    Update class_1.py

--- class_1.py
+++ class_1.py
@@ -1,1 +1,32 @@
+# 1
+first = 7
+second = 44.3
+print(first + second)
+print(first * second)
+print(first / second)
 
+# 2
+a = 8
+a = 17
+a = 9
+b = 6
+c = a + b
+b = c + a
+b = 8
+
+print(f"a = {a}, b = {b}, c = {c}")
+
+# 3
+# There is no difference for Python between "john" and 'john'
+
+# 4
+my_number = 5 + 5
+print(f"result is: {my_number}")
+
+# 5
+# The output will be 7 because we changed float y = 2.36 to int and it became 2
+
+# 6
+a = 8
+b = "123"
+print(a + int(b))
