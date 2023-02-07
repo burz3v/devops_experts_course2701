4ddee97059c990868a19a91964cd86f47291960a
Alexander Burtsev <97345685+burz3v@users.noreply.github.com>
2023-02-07 21:29:10 +0200

    Update class_1.py

--- class_1.py
+++ class_1.py
@@ -30,3 +30,53 @@ print(f"result is: {my_number}")
 a = 8
 b = "123"
 print(a + int(b))
+
+# Additional exercises
+# 1
+a = 5
+b = 7
+c = a
+a = b
+b = c
+print(f"a = {a}, b = {b}")
+
+# 2
+a = 5
+b = 7
+a = a + b
+b = a - b
+a = a - b
+print(f"a = {a}, b = {b}")
+
+# 3
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the second number: "))
+c = (a * a) + (b * b)
+print(f"Sum of first and second numbers cubes: {c}")
+
+# 4
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the second number: "))
+c = int(input("Please enter the third number: "))
+if a == b:
+    print(a)
+elif b == c:
+    print(b)
+elif c == a:
+    print(c)
+else:
+    print("No equal nubmers there.")
+
+# 5
+numbers_list = []
+
+first_number = int(input("Please enter the first number: "))
+second_number = int(input("Please enter the second number: "))
+third_number = int(input("Please enter the third number: "))
+
+numbers_list.append(first_number)
+numbers_list.append(second_number)
+numbers_list.append(third_number)
+numbers_list.sort()
+
+print(f"The smallest number is {numbers_list[0]}")
