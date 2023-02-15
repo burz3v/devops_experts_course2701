815d95cef78f714019beb2f5ce5b73730b0edfd6
Alexander Burtsev <97345685+burz3v@users.noreply.github.com>
2023-02-15 21:07:14 +0200

    Update class_2.py

--- class_2.py
+++ class_2.py
@@ -1,1 +1,98 @@
+# # 1
+#
+# x = int(input("Please enter the first number: "))
+# y = int(input("Please enter the second number: "))
+#
+# if x > y:
+#     print("BIG")
+# else:
+#     print("small")
 
+# # 2
+#
+# for x in range(5):
+#     print(f"The iteration number is {x + 1}")
+
+# # 3
+#
+# x = int(input("Please enter the number from 1 to 4: "))
+#
+# if x == 1:
+#     print("Summer")
+# elif x == 2:
+#     print("Winter")
+# elif x == 3:
+#     print("Fall")
+# elif x == 4:
+#     print("Spring")
+# else:
+#     print("Does not exists")
+
+# # 4
+#
+# # a: the loop runs 10 times
+# # b: 10 is printed last
+
+# # 5
+#
+# age = 30
+# first_letter_of_last_name = "B"
+# currency = 3.5
+# is_fly_aboard = True
+# apartment = 5
+# print(age, first_letter_of_last_name, currency, is_fly_aboard, apartment)
+# print(float(age) + currency)
+
+# # 6
+#
+# phone_number = int(input("Please enter your phone number: "))
+# print(f"Phone number {phone_number}")
+
+# # 7
+#
+# def print_hello():
+#     print("Hello")
+#
+# def calculate():
+#     print(float(5) + 3.2)
+
+# # 8
+#
+# def print_your_name(name):
+#     print(f"Your name is {name}")
+#
+# print_your_name("Alex")
+#
+# def divided_number(x):
+#     result = x / 2
+#     print(result)
+#
+# divided_number(float(4))
+
+# # 9
+#
+# def two_numbers(x, y):
+#     sum = x + y
+#     return sum
+#
+# print(two_numbers(2, 3))
+#
+# def two_strings(x, y):
+#     one_string = x + " " + y
+#     return one_string
+#
+# print(two_strings("Alexander", "Burtsev"))
+
+# # 12
+#
+# def get_number():
+#     number = list(input("number: "))
+#     return number
+#
+# def sum_of_digits(numbers):
+#     total = 0
+#     for number in numbers:
+#         total += int(number)
+#     return total
+#
+# print(f"The sum of numbers is: {sum_of_digits(get_number())}")
